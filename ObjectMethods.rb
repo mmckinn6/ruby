55e71e19fb3eedea22714c7a173c5ba938832320
mmckinn6 <48704878+mmckinn6@users.noreply.github.com>
2022-07-27 14:45:33 -0400

    Add files via upload

---
+++ ObjectMethods.rb
@@ -0,0 +1,13 @@
+p "hello world".length
+
+p "hello world".upcase
+
+foo = "hello world"
+p foo.length
+p foo.upcase
+p 10.next
+p -1.next
+
+puts "Hello world"
+puts "Hello world".inspect
+puts 50.inspect
