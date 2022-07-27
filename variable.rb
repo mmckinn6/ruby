a9047e3b131c46e0596dd08aebd7d98b89d84220
mmckinn6 <48704878+mmckinn6@users.noreply.github.com>
2022-07-27 14:31:26 -0400

    Add files via upload

---
+++ variable.rb
@@ -0,0 +1,33 @@
+# Variables
+first_name = "Matt"
+last_name = "McKinnon"
+cool = true
+age = 25 + 2
+favorite_number = 7
+# Variables
+
+puts first_name + " " + last_name # Prints "Matt McKinnon"
+
+puts cool 
+
+puts age    
+
+
+
+# Parallel Variable Assignment
+
+a = 10
+b = 20
+c = 30
+
+p a, b, c
+
+p a * b * c
+
+
+
+# Swapping Variable Values
+
+a = b
+
+p a + b
